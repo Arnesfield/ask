420f5d0543b24872e58b8cf0ab34ebbb58d8e5c1
Jefferson Rylee <rylee.jeff385@gmail.com>
2021-08-29 21:29:18 +0800

    Add doc comments

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,74 @@
 import { createInterface, Interface } from 'readline';
 
+/** Callback arguments for Ask. */
 export interface AskCallbackArgs {
+  /** The current prompt attempt. Starts at `0`. */
   iteration: number;
+  /** The previous answer. Contains an empty string for the first iteration. */
   previousAnswer: string;
 }
 
+/** Ask options. */
 export interface AskOptions {
+  /** Trims the answer before `format(answer, args)` is called. */
   trim?: boolean;
+  /**
+   * Format the answer.
+   * @param answer The answer.
+   * @param args The callback arguments.
+   * @returns The formatted answer.
+   */
   format?(answer: string, args: AskCallbackArgs): string;
   /** List of valid answers or a callback that checks if the answer is valid. */
   accept?: boolean | string[] | ((answer: string) => boolean);
 }
 
+/** Ask options with question. */
 export interface AskWithQuestionOptions extends AskOptions {
+  /** The question. */
   question: string;
 }
 
+/**
+ * Ask callback options.
+ * @param args The callback arguments.
+ * @returns The options with question.
+ */
 export type AskCallbackOptions = (
   args: AskCallbackArgs
 ) => AskWithQuestionOptions;
 
+/** Ask function. */
 export interface AskFunction {
+  /**
+   * Ask for user input.
+   * @param question The question.
+   * @param options Additional options.
+   * @returns The answer.
+   */
   (question: string, options?: AskOptions): Promise<string>;
+  /**
+   * Ask for user input.
+   * @param callback The callback options.
+   * @returns The answer.
+   */
   (callback: AskCallbackOptions): Promise<string>;
 }
 
+/** Ask function with additional methods. */
 export interface Ask extends AskFunction {
+  /**
+   * Create a scoped call.
+   * The readline interface will close only after the `callback` is finished.
+   * @param callback The scoped callback function with the `ask` function.
+   * @returns The result of `callback`.
+   */
   scoped<T>(callback: (ask: AskFunction) => T): T;
+  /**
+   * Use a custom readline interface.
+   * @param rl The readline interface.
+   * @returns A new `Ask` function using the provided `rl`.
+   */
   use(rl: Interface): Ask;
 }
 
